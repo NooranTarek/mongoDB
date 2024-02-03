0d2582f3cdefb8560309aef152ec5c70ef7f655d
Nooran Tarek <102181986+NooranTarek@users.noreply.github.com>
2024-02-03 14:44:36 +0200

    Add files via upload

---
+++ lab2_books.js
@@ -0,0 +1,88 @@
+db.booksLib.aggregate([
+  {
+    $unwind: "$categories"
+  },
+  {
+    $group: {
+      _id: "$categories",
+      bookNum: { $sum: 1 }
+    }
+  },
+  {
+    $sort: { bookCount: 1 }
+  }
+]);
+//____________________________________________________
+db.booksLib.aggregate([
+  {
+    $unwind: "$authors"
+  },
+  {
+    $project: {
+      title: 1,
+      authors: 1,
+      pageCount: 1,
+    }
+  }
+]);
+//____________________________________________________
+db.booksLib.aggregate([
+  {
+    $addFields: {
+      isNewBook: {
+        $gte: ["$publishedDate", 2023]
+      }
+    }
+  },
+  {
+    $limit: 5
+  },
+  {
+    $sort: { _id: 1 }
+  }
+]);
+//____________________________________________________
+db.booksLib.aggregate([
+  {
+    $unwind: "$authors"
+  },
+  {
+    $project: {
+      bookDetails: {
+        $concat: [
+          "$title",
+          " by ",
+          "$authors"
+        ]
+      }
+    }
+  }
+]);
+//____________________________________________________
+db.booksLib.aggregate([
+  {
+    $sort: { _id: 1 }
+  },
+  {
+    $skip: 5
+  }
+]);  
+//____________________________________________________
+db.booksLib.aggregate([
+  {
+    $project: {
+      title: 1,
+      publishedDate: {
+        $dateToString: {
+          format: "%Y-%m-%d",
+          date: "$publishedDate"
+        }
+      }
+    }
+  }
+]);
+
+  
+  
+  
+  
